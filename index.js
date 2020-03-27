e112f96474f8dcd37b42bc070a9f8390109d7289
Anders Windelhed <anders.windelhed@bonnierbroadcasting.com>
2020-03-27 21:16:37 +0100

    Updating lint rules

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const eslintRules = {
     jest: true,
   },
   rules: {
+    'no-use-before-define': ['error', { functions: true, variables: false }], // Enabling use of functions before defined to be able to keep the most important functions at the top of the file
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }], // Allowing jsx in js-files
+    'react/jsx-one-expression-per-line': 'off', // Allowing multiple expressions per line for example <span><h1></h1><p></p></span>
     semi: ['error', 'never'],
   },
 }
