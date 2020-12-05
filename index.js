b623edd66df747dc0d0212cd0521c51b55c240e4
Anders Windelhed <anders.windelhed@tv4.se>
2020-12-05 18:19:16 +0100

    Updating lint rules

--- index.js
+++ index.js
@@ -15,8 +15,24 @@ const eslintRules = {
     'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
     'consistent-return': 'off', // Allowing guard clauses returning undefined
     'no-await-in-loop': 'off', // Allowing await in loops
+    'no-continue': 'off', // Allowing continue in loops
     'no-param-reassign': 'off', // Allowing reassigning of params
     'no-plusplus': 'off', // Allowing the unary operators ++ and --
+    'no-restricted-syntax': [ // We allow for-of-loops
+      'error',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
     'no-use-before-define': ['error', { functions: true, variables: false }], // Enabling use of functions before defined to be able to keep the most important functions at the top of the file
     'object-curly-newline': 'off', // Allowing choice of whether to use newline or not
     'react/forbid-prop-types': 'off', // Allowing objects as prop types
