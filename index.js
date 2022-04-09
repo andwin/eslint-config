1d6e004557fe64e44037f7afef2dcb899bac9d94
Anders Windelhed <anders.windelhed@tv4.se>
2022-04-09 21:49:11 +0200

    Allowing arrow functions for components

--- index.js
+++ index.js
@@ -50,6 +50,10 @@ const eslintRules = {
     'no-use-before-define': ['error', { functions: true, variables: false }], // Enabling use of functions before defined to be able to keep the most important functions at the top of the file
     'object-curly-newline': 'off', // Allowing choice of whether to use newline or not
     'react/forbid-prop-types': 'off', // Allowing objects as prop types
+    'react/function-component-definition': ['error', { // Allowing arrow functions for components
+      namedComponents: 'arrow-function',
+      unnamedComponents: 'arrow-function',
+    }],
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }], // Allowing jsx in js-files
     'react/jsx-one-expression-per-line': 'off', // Allowing multiple expressions per line for example <span><h1></h1><p></p></span>
     'react/jsx-props-no-spreading': 'off', // Allowing props spreading
